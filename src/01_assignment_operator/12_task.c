d73c67c3df0eb61380e108b8e47aa161d7099283
Neto test <ki.dmitriev.yandex@gmail.com>
2026-02-08 02:22:53 +0300

    feat: add solution for task 8-13 (assignment operators)

---
+++ src/01_assignment_operator/12_task.c
@@ -0,0 +1,14 @@
+int main()
+{
+    // Объявляем и инициализируем переменные
+    int notebooks_number = 3; // Количество тетрадей
+    int covers_number = 2;    // Количество обложек
+    int pensils_number = 5;   // Количество карандашей
+
+    int notebook_price = 70; // Цена одной тетради
+    int cover_price = 20;    // Цена одной обложки
+    int pensil_price = 30;   // Цена одного карандаша
+
+    // Вычисление стоимости покупки
+    int total_cost = notebooks_number * notebook_price + covers_number * cover_price + pensils_number * pensil_price;
+}
---
+++ src/01_assignment_operator/13_task.c
@@ -0,0 +1,16 @@
+int main()
+{
+    // Объявление и инициализация переменных
+    double tomatoes_kg = 2.5;  // Количество помидоров в килограммах
+    double cucumbers_kg = 1.5; // Количество огурцов в килограммах
+    double dill_bunches = 3;   // Количество пучков укропа
+
+    // Цены товаров
+    double tomato_price = 250.70;   // Цена за один килограмм помидоров
+    double cucumber_price = 201.80; // Цена за один килограмм огурцов
+    double dill_price = 120;        // Цена за один пучок укропа
+
+    // Вычисление стоимости покупок
+
+    double total_cost = tomatoes_kg * tomato_price + cucumbers_kg * cucumber_price + dill_bunches * dill_price;
+}
